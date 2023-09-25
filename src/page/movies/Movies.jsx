28cd314842140a2454f9bb8e86b8270abbc86dc1
Serhiy8 <markovsm86@gmail.com>
2023-09-25 20:46:40 +0300

    Update Movies.jsx

--- src/page/movies/Movies.jsx
+++ src/page/movies/Movies.jsx
@@ -9,6 +9,7 @@ const URL = 'https://api.themoviedb.org/3/search/movie';
 const Movies = () => {
   const [inputValue, setInputValue] = useState('');
   const [dataMovies, setDataMovies] = useState([]);
+  const [notFound, setNotFounf] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get('query');
 
@@ -38,9 +39,12 @@ const Movies = () => {
   const searchMovies = async query => {
     try {
       const res = await fetchFromTMDbAPI(URL, { query });
-      if (res) {
-        setDataMovies(res.results);
+      if (res.total_results === 0) {
+        setNotFounf(true);
+        return;
       }
+      setDataMovies(res.results);
+      setNotFounf(false);
     } catch (error) {
       console.error('An error occurred while searching for movies:', error);
     }
@@ -57,7 +61,11 @@ const Movies = () => {
         />
         <FormButton type="submit">Search</FormButton>
       </FormSearch>
-      <MoviesList dataMovies={dataMovies} />
+      {notFound ? (
+        <div>Nothing was found for your request.</div>
+      ) : (
+        <MoviesList dataMovies={dataMovies} />
+      )}
     </ThumbForm>
   );
 };
