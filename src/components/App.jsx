874ee22d2d88bb7124c5dc761072d5aff12f3aee
Serhiy8 <markovsm86@gmail.com>
2023-09-25 16:04:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
+import MoviesDetails from 'page/MoviesDetails/MoviesDetails';
 const Home = lazy(() => import('../page/home/Home'));
 const Movies = lazy(() => import('../page/movies/Movies'));
 const Reviews = lazy(() => import('./review/Reviews'));
 const Cast = lazy(() => import('./cast/Cast'));
 const SharedLayout = lazy(() => import('./sharedLayout/SharedLayout'));
-const MoviesDetails = lazy(() => import('../page/MoviesDetails/MoviesDetails'));
+// const MoviesDetails = lazy(() => import('../page/MoviesDetails/MoviesDetails'));
 
 export const App = () => {
   return (
